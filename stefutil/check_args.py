50f51ad453456cf947b46d0c70a7d03eb11a5ab6
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2022-04-25 13:11:34 -0400

    Update: taxnomize

--- stefutil/check_args.py
+++ stefutil/check_args.py
@@ -1,25 +1,22 @@
+"""
+An easy, readable interface for checking string arguments as effectively enums
+
+Intended for high-level arguments instead of actual data processing as not as efficient
+"""
+
+
 from typing import List
 
-from ecg_transformer.util.util import *
+from stefutil.prettier import logi
+
+
+__all__ = ['CheckArg', 'ca']
 
 
 class CheckArg:
     """
     Raise errors when common arguments don't match the expected values
     """
-    dataset_names = [
-        'BIH-MVED', 'INCART',
-        'PTB-XL', 'PTB-Diagnostic',
-        'CSPC', 'CSPC-CinC', 'CSPC-Extra-CinC',
-        'G12EC', 'CHAP-SHAO', 'CODE-TEST'
-    ]
-    ptbxl_types = ['original', 'denoised']  # my de-noised version using the **other** PTB-XL paper approach
-
-    model_names = ['ecg-vit-debug', 'ecg-vit-tiny', 'ecg-vit-small', 'ecg-vit-base', 'ecg-vit-large']
-    optimizer = ['Adam', 'AdamW']
-    schedule = ['constant', 'cosine']
-
-    orients = ['v', 'h', 'vertical', 'horizontal']
 
     @staticmethod
     def check_mismatch(arg_type: str, arg_value: str, expected_values: List[str]):
@@ -27,48 +24,24 @@ class CheckArg:
             raise ValueError(f'Unexpected {logi(arg_type)}: '
                              f'expect one of {logi(expected_values)}, got {logi(arg_value)}')
 
-    @staticmethod
-    def check_dataset_name(dataset_name: str):
-        CheckArg.check_mismatch('Dataset Name', dataset_name, CheckArg.dataset_names)
-
-    @staticmethod
-    def check_ptbxl_type(type: str):
-        CheckArg.check_mismatch('PTB-XL dataset type', type, CheckArg.ptbxl_types)
-
-    @staticmethod
-    def check_model_name(model_name: str):
-        CheckArg.check_mismatch('Model Name', model_name, CheckArg.model_names)
-
-    @staticmethod
-    def check_optimizer(optimizer: str):
-        CheckArg.check_mismatch('Optimizer', optimizer, CheckArg.optimizer)
-
-    @staticmethod
-    def check_schedule(schedule: str):
-        CheckArg.check_mismatch('Schedule', schedule, CheckArg.schedule)
-
-    @staticmethod
-    def check_orient(orient: str):
-        CheckArg.check_mismatch('Bar Plot Orientation', orient, CheckArg.orients)
-
     def __init__(self):
-        self.d_name2func = dict(
-            dataset_name=CheckArg.check_dataset_name,
-            type=CheckArg.check_ptbxl_type,
-            model_name=CheckArg.check_model_name,
-            optimizer=CheckArg.check_optimizer,
-            schedule=CheckArg.check_schedule,
-            orient=CheckArg.check_orient
-        )
+        self.d_name2func = dict()
 
     def __call__(self, **kwargs):
         for k in kwargs:
             self.d_name2func[k](kwargs[k])
+
+    def cache_mismatch(self, display_name: str, attr_name: str, accepted_values: List[str]):
+        self.d_name2func[attr_name] = lambda x: CheckArg.check_mismatch(display_name, x, accepted_values)
 
 
 ca = CheckArg()
 
 
 if __name__ == '__main__':
-    type = 'original'
-    ca(type=type)
+    ca.cache_mismatch(
+        'Bar Plot Orientation', attr_name='bar_orient', accepted_values=['v', 'h', 'vertical', 'horizontal']
+    )
+
+    ori = 'v'
+    ca(bar_orient=ori)
