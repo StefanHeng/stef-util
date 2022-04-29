f487c57192834c369aa2d8156b9e4a013cc6ee14
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2022-04-29 13:29:26 -0400

    Chore: publish; Update: concurrency

--- stefutil/concurrency.py
+++ stefutil/concurrency.py
@@ -6,8 +6,9 @@ intended for (potentially heavy) data processing
 
 import os
 import concurrent.futures
-from typing import List, Iterable, Callable, TypeVar, Union
+from typing import List, Tuple, Iterable, Callable, TypeVar, Union
 
+import numpy as np
 from tqdm import tqdm
 
 
@@ -17,8 +18,11 @@ __all__ = ['conc_map', 'batched_conc_map']
 T = TypeVar('T')
 K = TypeVar('K')
 
+Map = Callable[[T], K]
+BatchedMap = Callable[[Tuple[List[T], int, int]], List[K]]
 
-def conc_map(fn: Callable[[T], K], it: Iterable[T], with_tqdm=False) -> Iterable[K]:
+
+def conc_map(fn: Map, it: Iterable[T], with_tqdm=False) -> List[K]:
     """
     Wrapper for `concurrent.futures.map`
 
@@ -33,23 +37,25 @@ def conc_map(fn: Callable[[T], K], it: Iterable[T], with_tqdm=False) -> Iterable
 
 
 def batched_conc_map(
-        # fn: Callable[[Tuple[List[T], int, int]], K],
-        fn: Callable[[T], K],
-        lst: List[T], n_worker: int = os.cpu_count(),
+        fn: Union[Map, BatchedMap],
+        lst: Union[List[T], np.array], n_worker: int = os.cpu_count(),
         batch_size: int = None,
-        with_tqdm: Union[bool, tqdm] = False
+        with_tqdm: Union[bool, tqdm] = False,
+        is_batched_fn: bool = False,
 ) -> List[K]:
     """
     Batched concurrent mapping, map elements in list in batches
+    Operates on batch/subset of `lst` elements given inclusive begin & exclusive end indices
 
     :param fn: A map function that operates on a single element
-        A version that operates on batch/subset of `lst` elements given inclusive begin & exclusive end indices
-            is created internally
     :param lst: A list of elements to map
     :param n_worker: Number of concurrent workers
     :param batch_size: Number of elements for each sub-process worker
         Inferred based on number of workers if not given
     :param with_tqdm: If true, progress bar is shown
+        progress is shown on an element-level if possible
+    :param is_batched_fn: If true, `conc_map` is called on the function passed in,
+        otherwise, A batched version is created internally
 
     .. note:: Concurrently is not invoked if too little list elements given number of workers
         Force concurrency with `batch_size`
@@ -65,26 +71,34 @@ def batched_conc_map(
         if with_tqdm:
             pbar = tqdm(total=len(lst)) if with_tqdm is True else with_tqdm
 
-        if with_tqdm:
-            def map_single(x):
-                ret = fn(x)
-                pbar.update(1)
+        if is_batched_fn:
+            def batched_map(lst__, s, e):
+                ret = fn(lst__[s:e])
+                if pbar:
+                    # update on an element level may not give a good estimate of completion if too large a batch
+                    pbar.update(e-s + 1)
                 return ret
         else:
-            map_single = fn
+            if with_tqdm:
+                def map_single(x):
+                    ret = fn(x)
+                    pbar.update(1)
+                    return ret
+            else:
+                map_single = fn
 
-        def batched_map(fnms_, s, e):
-            return [map_single(fnms_[i]) for i in range(s, e)]
-
-        # Expand the args
-        map_out = conc_map(
+            def batched_map(fnms_, s, e):
+                return [map_single(fnms_[i]) for i in range(s, e)]
+        map_out = conc_map(  # Expand the args
             lambda args_: batched_map(*args_), [(lst, s, e) for s, e in zip(strts, ends)], with_tqdm=False
         )
         for lst_ in map_out:
             lst_out.extend(lst_)
         return lst_out
     else:
-        # args = lst, 0, n
-        # return fn(*args)
         gen = tqdm(lst) if with_tqdm else lst
-        return [fn(x) for x in gen]
+        if is_batched_fn:
+            args = gen, 0, n
+            return fn(*args)
+        else:
+            return [fn(x) for x in gen]
