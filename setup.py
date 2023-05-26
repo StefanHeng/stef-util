79c255a8e00d38ad9d67a46a21dd96b12fb52b0c
Stefan Heng <stefan.hg@outlook.com>
2023-05-26 13:19:20 -0400

    New: eval res to sklearn df

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.22.4'
+VERSION = '0.22.5'
 DESCRIPTION = 'Machine Learning project startup utilities'
 LONG_DESCRIPTION = 'My commonly used utilities for machine learning projects'
 
@@ -13,12 +13,12 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     url='https://github.com/StefanHeng/stef-util',
-    download_url='https://github.com/StefanHeng/stef-util/archive/refs/tags/v0.22.4.tar.gz',
+    download_url='https://github.com/StefanHeng/stef-util/archive/refs/tags/v0.22.5.tar.gz',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'sty', 'colorama', 'pygments',
-        'numpy', 'pandas', 'torch', 'transformers',
+        'numpy', 'pandas', 'scikit-learn', 'torch', 'transformers',
         'matplotlib', 'seaborn', 'tensorboard',
         'tqdm', 'icecream'
     ],
--- stefutil/ml.py
+++ stefutil/ml.py
@@ -3,13 +3,16 @@ machine learning
 """
 
 import sys
-from typing import Union
+import math
+from typing import Tuple, Dict, Union, Iterable
 
+import pandas as pd
 import torch
+from sklearn.metrics import classification_report
 
 from stefutil.prettier import fmt_num
 
-__all__ = ['model_param_size', 'get_model_num_trainable_parameter', 'is_on_colab']
+__all__ = ['model_param_size', 'get_model_num_trainable_parameter', 'is_on_colab', 'eval_array2report_df']
 
 
 def get_torch_device():
@@ -29,3 +32,16 @@ def get_model_num_trainable_parameter(model: torch.nn.Module, readable: bool = T
 
 def is_on_colab() -> bool:
     return 'google.colab' in sys.modules
+
+
+def eval_array2report_df(
+        labels: Iterable, preds: Iterable, report_args: Dict = None, pretty: bool = True
+) -> Tuple[pd.DataFrame, float]:
+    report = classification_report(labels, preds, **(report_args or dict()))
+    if 'accuracy' in report:
+        acc = report['accuracy']
+    else:
+        vals = [v for k, v in report['micro avg'].items() if k != 'support']
+        assert all(math.isclose(v, vals[0], abs_tol=1e-8) for v in vals)
+        acc = vals[0]
+    return pd.DataFrame(report).transpose(), round(acc, 3) if pretty else acc
