f6460701873e61382aa4efc9ca2912fa05efac30
Stefan Heng <stefan.hg@outlook.com>
2024-02-05 10:35:57 -0500

    Chore: publish; Update/Fix: profile runtime write

--- stefutil/functional.py
+++ stefutil/functional.py
@@ -2,9 +2,11 @@
 function-level manipulation
 """
 import os
-from os.path import join as os_join
 import sys
 import time
+import pstats
+import cProfile
+from os.path import join as os_join
 from typing import Callable, Union
 
 from stefutil.prettier import *
@@ -14,10 +16,9 @@ __all__ = ['profile_runtime', 'RecurseLimit']
 
 
 def profile_runtime(
-        callback: Callable, sleep: Union[float, int] = None, write: bool = False, output_dir_name: str = None, output_file_name: str = None
-):
-    import cProfile
-    import pstats
+        callback: Callable, sleep: Union[float, int] = None, disable_stdout: bool = False,
+        write: bool = False, output_dir_name: str = None, output_file_name: str = None
+) -> pstats.Stats:
     profiler = cProfile.Profile()
     profiler.enable()
     callback()
@@ -25,9 +26,10 @@ def profile_runtime(
     stats = pstats.Stats(profiler).sort_stats('cumtime')
     if sleep:    # Sometimes, the top rows in `print_states` are now shown properly
         time.sleep(sleep)
-    stats.print_stats()
 
-    if write:
+    if not disable_stdout:
+        stats.print_stats()
+    if write or output_dir_name or output_file_name:
         now_ = now(for_path=True, fmt='short-full')
         fnm = f'{now_}_RuntimeProfile'
         if output_file_name:
@@ -35,7 +37,10 @@ def profile_runtime(
         if output_dir_name:
             os.makedirs(output_dir_name, exist_ok=True)
             fnm = os_join(output_dir_name, fnm)
-        stats.dump_stats(f'{fnm}.log')
+        with open(f'{fnm}.log', 'w') as f:
+            stats.stream = f
+            stats.print_stats()
+    return stats
 
 
 class RecurseLimit:
@@ -49,3 +54,13 @@ class RecurseLimit:
 
     def __exit__(self, kind, value, tb):
         sys.setrecursionlimit(self.old_limit)
+
+
+if __name__ == '__main__':
+    def test_profile_runtime():
+        def fib(n):
+            if n <= 1:
+                return n
+            return fib(n - 1) + fib(n - 2)
+        profile_runtime(lambda: fib(5), write=True)
+    test_profile_runtime()
