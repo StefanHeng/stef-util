edd721f4a70fd69c504a308467d48ee87c7ef8f1
Stefan Heng <stefan.hg@outlook.com>
2024-02-20 20:43:46 -0500

    Chore: publish; Update: stem parent as list

--- stefutil/os_n_file.py
+++ stefutil/os_n_file.py
@@ -5,7 +5,7 @@ os-related
 import os
 from os.path import join as os_join
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 
 __all__ = ['get_hostname', 'stem']
@@ -15,17 +15,20 @@ def get_hostname() -> str:
     return os.uname().nodename
 
 
-def stem(path: Union[str, Path], keep_ext=False, top_n: int = None) -> Union[str, Path]:
+def stem(
+        path: Union[str, Path], keep_ext=False, top_n: int = None, as_list: bool = False
+) -> Union[str, List[str]]:
     """
     :param path: A potentially full path to a file
     :param keep_ext: If True, file extensions is preserved
     :param top_n: If given, keep the top `top_n` parent directories
+    :param as_list: If True, return as a list
+        Relevant only when `top_n` is given
     :return: The file name, without parent directories
     """
     if top_n:
         ret = stem(path=path, keep_ext=keep_ext, top_n=None)
         if isinstance(path, Path):
-
             dirs = []
             for _ in range(top_n):
                 path = path.parent
@@ -34,7 +37,8 @@ def stem(path: Union[str, Path], keep_ext=False, top_n: int = None) -> Union[str
         else:
             dirs = path.split(os.sep)
             dirs = dirs[-top_n-1:-1]
-        return os_join(*dirs, ret)
+        dirs += [ret]
+        return dirs if as_list else os_join(*dirs)
     else:
         return os.path.basename(path) if keep_ext else Path(path).stem
 
@@ -46,6 +50,7 @@ if __name__ == '__main__':
         path_ = Path(path)
         print(path)
         print(stem(path, top_n=n))
+        print(stem(path, top_n=n, as_list=True))
         print(path_)
         print(stem(path_, top_n=n))
     check_stem()
